edbc81ade04b7df54e9e4656516091b448ec5650
Meet Rasania <meetrasania132@gmail.com>
2024-09-11 19:06:24 +0530

    Create firebase_api.dart
    updates

---
+++ lib/api/firebase_api.dart
@@ -0,0 +1,20 @@
+import 'package:firebase_messaging/firebase_messaging.dart';
+
+Future<void> onBackgroundMessage(RemoteMessage message) async {
+  print('Title : ${message.notification?.title}');
+  print('Body : ${message.notification?.body}');
+  print('Payload : ${message.data}');
+}
+
+class FirebaseApi{
+  final _firebaseMessaging = FirebaseMessaging.instance;
+
+  Future<void> initNotifications() async{
+    await _firebaseMessaging.requestPermission();
+    final token = await _firebaseMessaging.getToken();
+    print("token is $token");
+    FirebaseMessaging.onBackgroundMessage(onBackgroundMessage);
+  }
+
+
+}
